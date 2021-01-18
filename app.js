543810d1fe1a2ea2f641c3366611afd3f0aa61ad
ryusei <perusonaryu1127@gmail.com>
2021-01-19 01:51:32 +0900

    ボタンデザイン変更

--- app.js
+++ app.js
@@ -5,41 +5,54 @@ const list    = document.querySelector('.todos');
 const search  = document.querySelector('.search input');
 
 
+//HTML タスクリストテンプレートを作成
+const taskHtml = (taskData) => {
+    let taskCheckbox = '';
+    if(taskData.taskCheck == 0){
+        taskCheckbox = `<input type="checkbox" name="checkbox" class="checkbtn" >`;
+    }else if(taskData.taskCheck == 1){
+        taskCheckbox = `<input type="checkbox" name="checkbox" class="checkbtn"  checked >`;
+    }
+
+    const html =
+    `
+
+        <li  class="list-group-item p-0">
+            <div class="list-group-item d-flex justify-content-between align-items-center"  style="background-color:${taskData.color};" data-title='${taskData.task}'>
+                <div class="d-flex align-items-center">
+                    ${taskCheckbox}
+                    <h5 class="text-center title mb-0">${taskData.task}</h5>
+                </div>
+
+                <div data-title='${taskData.task}' class="d-flex align-items-center">
+
+                    <i class="far fa-2x fa-trash-alt delete"></i>
+                    
+                </div>
+            </div>
+            <p class="mb-4">${taskData.dateTime}</p>
+        </li>
+        
+
+    `
+
+    list.innerHTML += html;
+}
+
+
 (function(){
     //初期化処理
     //ローカルストレージに格納されている値を取得し、リストを生成する
     for(var key in localStorage){
         var taskData = JSON.parse(localStorage.getItem(key));
-        let taskCheckbox = '';
-        
-                
         if(taskData){
-            if(taskData.taskCheck == 0){
-
-                taskCheckbox = `<input type="checkbox" name="checkbox" class="checkbtn" >`;
-            }else if(taskData.taskCheck == 1){
-                taskCheckbox = `<input type="checkbox" name="checkbox" class="checkbtn"  checked >`;
-
-            }
-
-            list.innerHTML += `
-                <li class="list-group-item d-flex justify-content-between align-items-center mb-4" style="background-color:${taskData.color};">
-                    <div>
-                        <p class="text-center title">${taskData.task}</p>
-                        <p class="fon-s">${taskData.dateTime}</p>
-                    </div>
-
-                    <div data-title='${taskData.task}' class="d-flex align-items-center">
-
-                        <i class="far fa-2x fa-trash-alt delete mr-3"></i>
-                        ${taskCheckbox}
-                    </div>
-                </li>
-            `;
+            taskHtml(taskData);
         }
     }
 
 })();
+
+
 
 //日付時間取得
 const getDateTime = () => {
@@ -50,13 +63,12 @@ const getDateTime = () => {
     const dateTime = 
         (date.getMonth() + 1) + "月" + 
         date.getDate() + "日" + 
-        date.getHours() + "時" + 
-        date.getMinutes() + "分" + 
-        youbi[date.getDay()] + "曜日";
+        "(" + youbi[date.getDay()] + ")";
     
-    return dateTime;
-                     
+    return dateTime;                 
 } 
+
+
 
 //localStorageにtaskを保存
 const saveTaskToLocalStorage = (task, dataList) => {
@@ -75,37 +87,21 @@ const deleteTaskFromLocalStorage = task => {
     return;
 }
 
-//taskをHTML
+//taskを作成
 const createTodoList = (task,color) => {
-    // console.log(getDateTime());
     const dateTime = getDateTime();
-
-    const dataList = {
+    const taskData = {
         task: task,
         dateTime: dateTime,
         color:color,
         taskCheck:0,
     }
-    //HTML テンプレートを作成
-    const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center mb-4" style="background-color:${color};">
-            <div>
-                <p class="text-center title">${task}</p>
-                <p class="fon-s">${dateTime}</p>
-            </div>
-            <div data-title='${task}'>
-
-                <i class="far fa-2x fa-trash-alt delete"></i>
-                <input type="checkbox" name="checkbox" class="check checkbtn">
-
-            </div>
-        </li>
-    `;
-
-    list.innerHTML += html;
-    saveTaskToLocalStorage(task,dataList);
+    
+    taskHtml(taskData);
+    saveTaskToLocalStorage(task,taskData);
 }
 
+//タスク名でのフィルター機能
 const filterTasks = (term) =>  {
     Array.from(list.children)
         //フィルタ条件
@@ -117,15 +113,17 @@ const filterTasks = (term) =>  {
         .forEach((todo) => todo.classList.remove('filtered'));
 };
 
+
 search.addEventListener('keyup', e => {
     e.preventDefault();
+
     //空白削除かつ、小文字に変換(大文字・小文字の区別をなくす)
     const term = search.value.trim().toLowerCase();
-    // console.log(list.children[0].textContent.toLowerCase().includes(term));
 
     filterTasks(term);
 })
 
+//タスク追加イベント
 addTask.addEventListener('click', e => {
     //デフォルトのイベントを無効
     e.preventDefault();
@@ -144,22 +142,21 @@ addTask.addEventListener('click', e => {
 
 
 list.addEventListener('click',e => {
+
     //削除機能
     if(e.target.classList.contains('delete')){
-        e.target.parentElement.parentElement.remove();
-        const task = e.target.parentElement.getAttribute('data-title').trim();
+        e.target.parentElement.parentElement.parentElement.remove();
+        const task = e.target.parentElement.parentElement.getAttribute('data-title').trim();
         deleteTaskFromLocalStorage(task);
     }
 
 
     //taskチェック機能
-
     if(e.target.classList.contains('checkbtn')){
 
         const checkBox = e.target;
-        const key = e.target.parentElement.getAttribute('data-title').trim();
+        const key = e.target.parentElement.parentElement.getAttribute('data-title').trim();
         const taskData = JSON.parse(localStorage.getItem(key));
-        console.log(e.target.checked);
         if(checkBox.checked){
             taskData.taskCheck = 1;
             localStorage.setItem(key,JSON.stringify(taskData));
@@ -174,7 +171,7 @@ list.addEventListener('click',e => {
 
 
 
-
+//タスクカラー機能
 jQuery(function($){
     $("#picker").spectrum({
         color: "#cccccc", // 初期値
